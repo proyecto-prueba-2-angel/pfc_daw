53ca3d8e41aa94b3bc40677e64cb7f54ba1672d2
Emilio-HE <emilio.web.dev@gmail.com>
2024-09-17 11:31:12 +0200

    seguimos 24

--- components/WhatsApp.vue
+++ components/WhatsApp.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="whatsapp-button hover:shine-effect hidden sm:hidden md:block">
-    <a href="https://api.whatsapp.com/send?phone=627230250&text=Hola%20MegaHogar!" target="_blank" class="flex items-center justify-center bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-full shadow-lg">
+    <!-- <a href="https://api.whatsapp.com/send?phone=627230250&text=Hola%20MegaHogar!" target="_blank" class="flex items-center justify-center bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-full shadow-lg">
       <img src="/static/img/whatsapp_icon.png" alt="WhatsApp" class="h-6 w-6 mr-2">
       Chatea con nosotros
-    </a>
+    </a> -->
   </div>
 </template>
 
